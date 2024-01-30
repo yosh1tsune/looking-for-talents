95ba8c80d252be9b81b4ab6291d65030508449d1
Bruno Silva <brunorsilva_@hotmail.com>
2024-01-30 18:48:48 -0300

    update active storage tables

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_222905) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_01_30_195609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_222905) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -31,9 +30,16 @@ ActiveRecord::Schema.define(version: 2020_08_20_222905) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
+    t.string "checksum"
+    t.datetime "created_at", precision: nil, null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "addresses", force: :cascade do |t|
@@ -41,8 +47,8 @@ ActiveRecord::Schema.define(version: 2020_08_20_222905) do
     t.string "city"
     t.string "country"
     t.string "zipcode"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "neighborhood"
     t.string "state"
     t.string "addressable_type"
@@ -54,10 +60,10 @@ ActiveRecord::Schema.define(version: 2020_08_20_222905) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_candidates_on_email", unique: true
     t.index ["reset_password_token"], name: "index_candidates_on_reset_password_token", unique: true
   end
@@ -66,8 +72,8 @@ ActiveRecord::Schema.define(version: 2020_08_20_222905) do
     t.string "comment"
     t.bigint "profile_id", null: false
     t.bigint "headhunter_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["headhunter_id"], name: "index_comments_on_headhunter_id"
     t.index ["profile_id"], name: "index_comments_on_profile_id"
   end
@@ -77,8 +83,8 @@ ActiveRecord::Schema.define(version: 2020_08_20_222905) do
     t.string "document"
     t.string "description"
     t.bigint "headhunter_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email"
     t.string "phone"
     t.index ["headhunter_id"], name: "index_companies_on_headhunter_id"
@@ -92,8 +98,8 @@ ActiveRecord::Schema.define(version: 2020_08_20_222905) do
     t.boolean "currently_working", default: false
     t.string "company"
     t.string "role"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["profile_id"], name: "index_experiences_on_profile_id"
   end
 
@@ -101,10 +107,10 @@ ActiveRecord::Schema.define(version: 2020_08_20_222905) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "name"
     t.string "surname"
     t.index ["email"], name: "index_headhunters_on_email", unique: true
@@ -118,8 +124,8 @@ ActiveRecord::Schema.define(version: 2020_08_20_222905) do
     t.string "salary"
     t.string "grade"
     t.date "submit_end_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "status", default: 0, null: false
     t.bigint "headhunter_id"
     t.bigint "company_id", default: 0, null: false
@@ -135,8 +141,8 @@ ActiveRecord::Schema.define(version: 2020_08_20_222905) do
     t.text "professional_resume"
     t.boolean "highlighted", default: false
     t.bigint "candidate_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["candidate_id"], name: "index_profiles_on_candidate_id"
   end
 
@@ -149,8 +155,8 @@ ActiveRecord::Schema.define(version: 2020_08_20_222905) do
     t.text "bonuses"
     t.bigint "subscription_id", null: false
     t.bigint "opportunity_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "status", default: 0
     t.index ["opportunity_id"], name: "index_proposals_on_opportunity_id"
     t.index ["subscription_id"], name: "index_proposals_on_subscription_id"
@@ -159,8 +165,8 @@ ActiveRecord::Schema.define(version: 2020_08_20_222905) do
   create_table "servicing_headhunters", force: :cascade do |t|
     t.bigint "headhunter_id", null: false
     t.bigint "company_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["company_id"], name: "index_servicing_headhunters_on_company_id"
     t.index ["headhunter_id"], name: "index_servicing_headhunters_on_headhunter_id"
   end
@@ -172,13 +178,14 @@ ActiveRecord::Schema.define(version: 2020_08_20_222905) do
     t.boolean "highlighted"
     t.integer "status", default: 0
     t.text "feedback"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["candidate_id"], name: "index_subscriptions_on_candidate_id"
     t.index ["opportunity_id"], name: "index_subscriptions_on_opportunity_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "headhunters"
   add_foreign_key "comments", "profiles"
   add_foreign_key "companies", "headhunters"
