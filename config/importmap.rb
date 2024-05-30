e7cd1eb01f7f887364ea4a1e4c0c68c802d3a0fe
Bruno Silva <brunorsilva_@hotmail.com>
2024-05-30 01:47:30 -0300

    fix rubocop offenses and remove warning

--- config/importmap.rb
+++ config/importmap.rb
@@ -11,4 +11,4 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 
 pin_all_from 'app/javascript/packs', under: 'packs', to: 'packs'
-pin "@fortawesome/fontawesome-free", to: "@fortawesome--fontawesome-free.js" # @6.5.2
+pin '@fortawesome/fontawesome-free', to: '@fortawesome--fontawesome-free.js' # @6.5.2
