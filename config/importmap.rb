747c9fc9d4978afe72c00ab44dac3289c38e1a0b
Bruno Silva <brunorsilva_@hotmail.com>
2024-06-05 19:46:14 -0300

    update fontawesome-free import to http

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,7 +8,7 @@ pin '@rails/actiontext', to: 'actiontext.esm.js'
 pin 'trix'
 
 pin 'jquery', to: 'http://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js', preload: true
-pin '@fortawesome/fontawesome-free', to: '@fortawesome--fontawesome-free.js', preload: true # @6.5.2
+pin '@fortawesome/fontawesome-free', to: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/fontawesome.js'
 pin 'inputmask', to: 'https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/5.0.9/jquery.inputmask.js'
 pin 'toastr', to: 'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js'
 pin 'bootstrap', to: 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js'
