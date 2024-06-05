82ca2d07494f79d48e562ae2504de2f0c37e240a
Bruno Silva <brunorsilva_@hotmail.com>
2024-06-05 12:47:01 -0300

    remove fixture_path warning

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,7 +56,7 @@ end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = ["#{::Rails.root}/spec/fixtures"]
+  config.fixture_paths = ["#{::Rails.root}/spec/fixtures"]
 
   # Devise methods for tests
   config.include Warden::Test::Helpers
