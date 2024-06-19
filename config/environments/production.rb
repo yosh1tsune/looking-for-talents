7e135a925d992dba598f7708799d080fd7e70934
Bruno Silva <brunorsilva_@hotmail.com>
2024-06-19 18:43:44 -0300

    allow azure host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.hosts << "looking-for-talents.azurewebsites.net"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
