e9ef87645aa00bcf7f7570bcaf08d06559ec3a4f
Bruno Silva <brunorsilva_@hotmail.com>
2024-06-19 18:58:52 -0300

    allow azure host for production

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module LookingForTalents
 
     config.i18n.default_locale = :'pt-BR'
 
+    config.hosts << "looking-for-talents.azurewebsites.net"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
