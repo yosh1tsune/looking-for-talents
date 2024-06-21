741544e4e535a5d657e41a099393639321382a70
Bruno Silva <brunorsilva_@hotmail.com>
2024-06-21 15:42:32 -0300

    update jquery import to http

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,7 +7,7 @@ pin '@rails/activestorage', to: 'activestorage.esm.js'
 pin '@rails/actiontext', to: 'actiontext.esm.js'
 pin 'trix'
 
-pin 'jquery', to: 'http://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js', preload: true
+pin 'jquery', to: 'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js', preload: true
 pin '@fortawesome/fontawesome-free', to: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/fontawesome.js'
 pin 'inputmask', to: 'https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/5.0.9/jquery.inputmask.js'
 pin 'toastr', to: 'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js'
